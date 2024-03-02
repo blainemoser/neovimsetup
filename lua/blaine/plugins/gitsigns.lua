6484998faaf3d3dd6c9faeb0332d58b49d8c1218
Blaine Moser <blaine.moser@smokeci.com>
2024-03-02 02:41:16 +0200

    Gitsigns - blame

--- lua/blaine/plugins/gitsigns.lua
+++ lua/blaine/plugins/gitsigns.lua
@@ -2,6 +2,6 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		local signs = require("gitsigns")
-		signs.setup()
+		signs.setup({ auto_attach = true, attach_to_untracked = false, current_line_blame = true })
 	end,
 }
